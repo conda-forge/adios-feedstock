a583196607835eb7cdfa7ade3c48b582d1c02026
Axel Huebl <axel.huebl@plasma.ninja>
2018-10-31 13:29:57 +0100

    Revert Rpath/Link Changes
    Did not improve the downstream situation.
    Reverts #6 and #7.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,6 @@ if [[ ${target_platform} =~ osx.* ]]; then
 
     # remove -lrt
     sed -i '.bak' 's/ -lrt//g' $SRC_DIR/wrappers/numpy/Makefile
-else
-    # downstream linkage with shared libs to our dependencies
-    # https://github.com/conda-forge/adios-feedstock/pull/6#issuecomment-432995338
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 fi
 
 # Python3 fixes
@@ -17,11 +13,6 @@ if [[ "${PY_VER}" =~ 3 ]]
 then
   find $SRC_DIR/utils -name "*.py" -exec 2to3 -w -n {} \;
 fi
-
-# avoid linker issues with out dependencies (blosc, bzip2) in
-# downstream packages
-# https://github.com/conda/conda-docs/pull/624
-autoreconf -vfi
 
 # configure
 export LIBRARY_PATH="$PREFIX/lib"
